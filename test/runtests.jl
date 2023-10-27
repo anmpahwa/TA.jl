8365a72c3fe7f30e91be834111949206b56ec510
anmol1104 <anmpahwa@ucdavis.edu>
2023-10-26 17:46:47 -0700

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,16 @@ using Test
 @testset "TAPFW" begin
     assignment = assigntraffic(; method=:tapfw, network="Anaheim", assignment=:UE, tol=1e-50, maxiters=5, maxruntime=300, log=:off)
     report = assignment[:report]
-    rgₛ = report[begin, :LOG₁₀RG]
-    rgₑ = report[end, :LOG₁₀RG]
+    s, e = 1, size(report,1)
+    rgₛ = report[s, :LOG₁₀RG]
+    rgₑ = report[e, :LOG₁₀RG]
     @test rgₑ < rgₛ
 
     assignment = assigntraffic(; method=:tapfw, network="Anaheim", assignment=:SO, tol=1e-50, maxiters=5, maxruntime=300, log=:off)
     report = assignment[:report]
-    rgₛ = report[begin, :LOG₁₀RG]
-    rgₑ = report[end, :LOG₁₀RG]
+    s, e = 1, size(report,1)
+    rgₛ = report[s, :LOG₁₀RG]
+    rgₑ = report[e, :LOG₁₀RG]
     @test rgₑ < rgₛ
 end
 
@@ -21,14 +23,16 @@ end
 @testset "TAFFW" begin
     assignment = assigntraffic(; method=:taffw, network="Anaheim", assignment=:UE, tol=1e-50, maxiters=5, maxruntime=300, log=:off)
     report = assignment[:report]
-    rgₛ = report[begin, :LOG₁₀RG]
-    rgₑ = report[end, :LOG₁₀RG]
+    s, e = 1, size(report,1)
+    rgₛ = report[s, :LOG₁₀RG]
+    rgₑ = report[e, :LOG₁₀RG]
     @test rgₑ < rgₛ
 
     assignment = assigntraffic(; method=:taffw, network="Anaheim", assignment=:SO, tol=1e-50, maxiters=5, maxruntime=300, log=:off)
     report = assignment[:report]
-    rgₛ = report[begin, :LOG₁₀RG]
-    rgₑ = report[end, :LOG₁₀RG]
+    s, e = 1, size(report,1)
+    rgₛ = report[s, :LOG₁₀RG]
+    rgₑ = report[e, :LOG₁₀RG]
     @test rgₑ < rgₛ
 end
 
@@ -36,14 +40,16 @@ end
 @testset "TACFW" begin
     assignment = assigntraffic(; method=:tacfw, network="Anaheim", assignment=:UE, tol=1e-50, maxiters=5, maxruntime=300, log=:off)
     report = assignment[:report]
-    rgₛ = report[begin, :LOG₁₀RG]
-    rgₑ = report[end, :LOG₁₀RG]
+    s, e = 1, size(report,1)
+    rgₛ = report[s, :LOG₁₀RG]
+    rgₑ = report[e, :LOG₁₀RG]
     @test rgₑ < rgₛ
 
     assignment = assigntraffic(; method=:tacfw, network="Anaheim", assignment=:SO, tol=1e-50, maxiters=5, maxruntime=300, log=:off)
     report = assignment[:report]
-    rgₛ = report[begin, :LOG₁₀RG]
-    rgₑ = report[end, :LOG₁₀RG]
+    s, e = 1, size(report,1)
+    rgₛ = report[s, :LOG₁₀RG]
+    rgₑ = report[e, :LOG₁₀RG]
     @test rgₑ < rgₛ
 end
 
@@ -51,13 +57,15 @@ end
 @testset "TAPAS" begin
     assignment = assigntraffic(; method=:tapas, network="Anaheim", assignment=:UE, tol=1e-50, maxiters=5, maxruntime=300, log=:off)
     report = assignment[:report]
-    rgₛ = report[begin, :LOG₁₀RG]
-    rgₑ = report[end, :LOG₁₀RG]
+    s, e = 1, size(report,1)
+    rgₛ = report[s, :LOG₁₀RG]
+    rgₑ = report[e, :LOG₁₀RG]
     @test rgₑ < rgₛ
 
     assignment = assigntraffic(; method=:tapas, network="Anaheim", assignment=:SO, tol=1e-50, maxiters=5, maxruntime=300, log=:off)
     report = assignment[:report]
-    rgₛ = report[begin, :LOG₁₀RG]
-    rgₑ = report[end, :LOG₁₀RG]
+    s, e = 1, size(report,1)
+    rgₛ = report[s, :LOG₁₀RG]
+    rgₑ = report[e, :LOG₁₀RG]
     @test rgₑ < rgₛ
 end
